1ff08244778c00219cb4c4c22602c11d7c87e303
Alexi Turcotte <alexi@MAC.local>
2025-07-18 22:24:23 +0200

    transformation and configuration

--- src/Spreadsheet.tsx
+++ src/Spreadsheet.tsx
@@ -1,14 +1,14 @@
+import { memo } from "react";
 import { useCallback, useState } from "react";
 import Cell from "./Cell";
-
-type CellLocation = { row: number; col: number };
-
-function Spreadsheet() {
+type CellLocation = {
+  row: number;
+  col: number;
+};
+const Spreadsheet = memo(function Spreadsheet() {
   // TODO decide on the data structure for the sheet
   const [cellValues, setCellValues] = useState<Record<string, string>>({});
-  const [sheet, setSheet] = useState<string[][]>(() =>
-    new Array(20).fill(new Array(20).fill(""))
-  );
+  const [sheet, setSheet] = useState<string[][]>(() => new Array(60).fill(new Array(60).fill("")));
   const [selectedCell, setSelectedCell] = useState<CellLocation | null>(null);
 
   // TODO expand the sheet to 10000x10000 (buttons to go farther)
@@ -17,12 +17,11 @@ function Spreadsheet() {
   function translateColumnIndexToName(index: number): string {
     let name = "";
     while (index >= 0) {
-      name = String.fromCharCode((index % 26) + 65) + name;
+      name = String.fromCharCode(index % 26 + 65) + name;
       index = Math.floor(index / 26) - 1;
     }
     return name;
   }
-
   function translateColumnNameToIndex(name: string): number {
     let index = 0;
     for (let i = 0; i < name.length; i++) {
@@ -36,15 +35,16 @@ function Spreadsheet() {
     if (!value.startsWith("=")) return value;
     const matches = value.match(/([A-Z]+\d+)/g); // cell names
     const operations = value.split(/[A-Z]+\d+/g); // everything else
-    console.log({ matches, operations });
-    if (matches === null || operations[operations.length - 1] !== "")
-      return "Incorrect formula!";
+    console.log({
+      matches,
+      operations
+    });
+    if (matches === null || operations[operations.length - 1] !== "") return "Incorrect formula!";
     let result = 0;
     matches.forEach((val, index) => {
       // TODO add proper order of operations
       const [col, row] = val.match(/([A-Z]+)(\d+)/)!.slice(1);
-      const cellValue =
-        cellValues[`${parseInt(row) - 1},${translateColumnNameToIndex(col)}`];
+      const cellValue = cellValues[`${parseInt(row) - 1},${translateColumnNameToIndex(col)}`];
       if (cellValue === undefined) return "Incorrect formula!";
       if (index === 0) {
         result = parseFloat(parseCellValue(cellValue));
@@ -69,52 +69,36 @@ function Spreadsheet() {
     });
     return result.toString();
   }
-
-  function updateValue(newValue: string, row: number, col: number): void {
-    setCellValues((oldCellValues) => {
-      const newCellValues = { ...oldCellValues };
+  const updateValue = useCallback(function updateValue(newValue: string, row: number, col: number) {
+    setCellValues(oldCellValues => {
+      const newCellValues = {
+        ...oldCellValues
+      };
       newCellValues[`${row},${col}`] = newValue;
       return newCellValues;
     });
-  }
-
-  function selectCell(row: number, col: number): void {
-    setSelectedCell({ row, col });
-  }
-
-  return (
-    <div className="spreadsheet-wrapper">
+  }, [setCellValues]);
+  const selectCell = useCallback(function selectCell(row: number, col: number) {
+    setSelectedCell({
+      row,
+      col
+    });
+  }, [setSelectedCell]);
+  return <div className="spreadsheet-wrapper">
       <div className="spreadsheet">
         <div className="row header">
           <div className="cell header" key={-1}></div>
-          {sheet[0].map((col, j) => (
-            <div className="cell header" key={j}>
+          {sheet[0].map((col, j) => <div className="cell header" key={j}>
               {translateColumnIndexToName(j)}
-            </div>
-          ))}
+            </div>)}
         </div>
-        {sheet.map((row, i) => (
-          <div key={i} className="row">
+        {sheet.map((row, i) => <div key={i} className="row">
             <div className="cell header" key={-1}>
               {i + 1}
             </div>
-            {row.map((cell, j) => (
-              <Cell
-                key={j}
-                value={cellValues[`${i},${j}`] || ""}
-                displayValue={parseCellValue(cellValues[`${i},${j}`] || "")}
-                updateValue={updateValue}
-                selectCell={selectCell}
-                isSelected={selectedCell?.row === i && selectedCell?.col === j}
-                row={i}
-                col={j}
-              />
-            ))}
-          </div>
-        ))}
+            {row.map((cell, j) => <Cell key={j} value={cellValues[`${i},${j}`] || ""} displayValue={parseCellValue(cellValues[`${i},${j}`] || "")} updateValue={updateValue} selectCell={selectCell} isSelected={selectedCell?.row === i && selectedCell?.col === j} row={i} col={j} />)}
+          </div>)}
       </div>
-    </div>
-  );
-}
-
+    </div>;
+});
 export default Spreadsheet;
